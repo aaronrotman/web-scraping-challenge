073a54d53e103a5707e326bee48057ea13c3b969
Aaron Rotman <58377413+aaronrotman@users.noreply.github.com>
2020-05-26 00:06:12 -0700

    All data scrapes successfully except the latest news headline

--- mission_to_mars/test.py
+++ mission_to_mars/test.py
@@ -8,25 +8,59 @@ import time
 
 
 def scrape():
+    # MARS FEATURED IMAGE
+    feat_img_base_url = "https://www.jpl.nasa.gov/"
+    jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+
+    # Chrome driver setup
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+    try:
+        browser.visit(jpl_url)
+        html = browser.html
+
+        soup = bs(html, "html.parser")
+
+        print(soup.prettify())
+
+        feat_image = soup.find('div', class_="carousel_items")
+        feat_img_url = feat_image.a['data-link']
+
+        # Navigate to article page for the featured image
+        browser.visit(f"{feat_img_base_url}{feat_img_url}")
+
+        feat_img_html = browser.html
+        feat_soup = bs(feat_img_html, "html.parser")
 
 
+        img_fig = feat_soup.find('figure', class_='lede')
+        img_fig_url = img_fig.a['href']
+
+        # Close the browser
+        browser.quit()
+    except Exception as e:
+        print(e)
+        # Close the browser
+        browser.quit()
     # WEATHER
     url = 'https://twitter.com/marswxreport?lang=en'
+    try:
+        tweet_response = requests.get(url)
+        print(tweet_response)
 
-    tweet_response = requests.get(url)
-    print(tweet_response)
+        tweet_soup = bs(tweet_response.text, 'lxml')
+        print(tweet_soup.prettify())
 
-    tweet_soup = bs(tweet_response.text, 'lxml')
-    print(tweet_soup.prettify())
-
-    # weather_tweet = tweet_soup.find_all("span", class_="css-901oao css-16my406 r-1qd0xha r-ad9z0x r-bcqeeo r-qvutc0")
-    weather_list = []
-    weather_tweet = tweet_soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
-    for weather in weather_tweet:
-        weather_list.append(weather)
-    mars_weather = weather_list[0]
-    mars_weather = mars_weather.replace('\n', ' ')
-
+        weather_list = []
+        weather_tweet = tweet_soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text")
+        for weather in weather_tweet:
+            weather_list.append(weather)
+        mars_weather = weather_list[0]
+        mars_weather = mars_weather.replace('\n', ' ')
+    except Exception as e:
+        print(e)
+        # Close the browser
+        browser.quit()
 
     # FACT TABLE
     # Store the url
@@ -46,10 +80,51 @@ def scrape():
 
     html_table = fact_table.to_html()
 
+    # HEMISPHERE IMAGE URLS
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path, headless=False)
+
+    # Store the URL
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    # Go to website
+    browser.visit(url)
+
+    html = browser.html
+    soup = bs(html, 'lxml')
+
+    image_links = soup.find_all('div', class_='description')
+    hemi_base_url = 'https://astrogeology.usgs.gov'
+    hemisphere_image_urls = []
+
+    for link in image_links:
+        try:
+            print(f'Visiting: {link.h3.text}')
+            time.sleep(1)
+            print('Visiting...')
+            browser.click_link_by_partial_text(link.h3.text)
+            time.sleep(1)
+            
+            html = browser.html
+            soup = bs(html, 'lxml')
+
+            title = soup.find('h2', class_='title')
+            
+            image = soup.find('img', class_='wide-image')
+            print(image['src'])
+            hemisphere_image_urls.append({'title': title.text, 'img_url': f"{hemi_base_url}{image['src']}"})
+            print('Data scraped')
+            browser.visit(url)
+        except Exception as e:
+            print(e)
+            browser.visit(url)
+    # Close the browser
+    browser.quit()
+
     # Dictionary of scraping results
     mars_dict = {
         'fact_table': html_table,
-        'weather': mars_weather
+        'weather': mars_weather,
+        'hemisphere_image_urls': hemisphere_image_urls,
+        'feat_image_url': f"{feat_img_base_url}{img_fig_url}"
     }
-
     return mars_dict
